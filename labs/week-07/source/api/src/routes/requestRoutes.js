913fa4180629870410b7395ed60a1323fd7f1b57
KittitatK <skhoogkhogsung@gmail.com>
2026-09-15 22:04:38 +0700

    LAB07 : ADD CP15 CP16

--- labs/week-07/source/api/src/routes/requestRoutes.js
+++ labs/week-07/source/api/src/routes/requestRoutes.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import * as controller from '../controllers/requestController.js';
 import { asyncHandler } from '../middleware/errorHandler.js';
+import { validateRequest } from '../middleware/validateRequest.js';
 
 const router = express.Router();
 
 // นำ asyncHandler() ไปครอบฟังก์ชันของ controller แบบนี้
 router.get('/', asyncHandler(controller.listRequests));
 router.get('/:id', asyncHandler(controller.getRequest));
-router.post('/', asyncHandler(controller.createRequest));
+router.post('/', validateRequest, asyncHandler(controller.createRequest));
 router.post('/reset', asyncHandler(controller.resetRequests));
 router.put('/:id', asyncHandler(controller.updateRequestStatus));
 router.delete('/:id', asyncHandler(controller.deleteRequest));
